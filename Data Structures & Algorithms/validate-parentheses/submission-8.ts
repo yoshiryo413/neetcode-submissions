8ea9cd43068c6e819b9982951f1c07dc1c5a3390
yoshiryo413 <61876987+yoshiryo413@users.noreply.github.com>
2026-06-08 22:28:03 +0900

    Add: validate-parentheses - submission-8

---
+++ Data Structures & Algorithms/validate-parentheses/submission-8.ts
@@ -0,0 +1,35 @@
+class Solution {
+    /**
+     * @param {string} s
+     * @return {boolean}
+     */
+    isValid(s: string): boolean {
+
+        if (s.length < 2) return false
+
+        let temp = []
+        const pair = new Map([
+            [')', '('],
+            [']', '['],
+            ['}', '{'],
+        ]);
+
+        for (let i = 0; i < s.length; i++) {
+            if (!pair.has(s[i])) {
+                temp.push(s[i]);
+            /// 最初に閉じ括弧が来た場合
+            } else if (pair.has(s[i]) && temp.length == 0) {
+                return false;
+            } else if (pair.has(s[i]) && temp.length != 0) {
+                let val = pair.get(s[i]);
+                let stack = temp[temp.length - 1];
+                if (val == stack) {
+                    temp.pop();
+                } else {
+                    return false;
+                }
+            } 
+        }
+        return temp.length == 0;
+    }
+}
